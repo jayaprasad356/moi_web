ff59476cec1a29373754bd804ae4db88bf2fcd4c
divaharsha <divakarvan03@gmail.com>
2022-09-14 14:21:07 +0530

    initial

---
+++ home.php
@@ -0,0 +1,121 @@
+<?php session_start();
+
+include_once('includes/custom-functions.php');
+include_once('includes/functions.php');
+$function = new custom_functions;
+
+// set time for session timeout
+$currentTime = time() + 25200;
+$expired = 3600;
+// if session not set go to login page
+if (!isset($_SESSION['username'])) {
+    header("location:index.php");
+}
+// if current time is more than session timeout back to login page
+if ($currentTime > $_SESSION['timeout']) {
+    session_destroy();
+    header("location:index.php");
+}
+// destroy previous session timeout and create new one
+unset($_SESSION['timeout']);
+$_SESSION['timeout'] = $currentTime + $expired;
+$function = new custom_functions;
+include "header.php";
+?>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Moi App - Dashboard</title>
+</head>
+
+<body>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>Home</h1>
+            <ol class="breadcrumb">
+                <li>
+                    <a href="home.php"> <i class="fa fa-home"></i> Home</a>
+                </li>
+            </ol>
+        </section>
+        <section class="content">
+            <div class="row">
+                <div class="col-lg-3 col-xs-6">
+                    <div class="small-box bg-aqua">
+                        <div class="inner">
+                            <h3><?php
+                            $sql = "SELECT * FROM users";
+                            $db->sql($sql);
+                            $res = $db->getResult();
+                            $num = $db->numRows($res);
+                            echo $num;
+                             ?></h3>
+                            <p>பயனாளிகௗஂ</p>
+                        </div>
+                        <div class="icon"><i class="fa fa-user"></i></div>
+                        <a href="users.php" class="small-box-footer">மேலும் அறிக<i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-xs-6">
+                    <div class="small-box bg-yellow">
+                        <div class="inner">
+                        <h3><?php
+                            $sql = "SELECT * FROM functions";
+                            $db->sql($sql);
+                            $res = $db->getResult();
+                            $num = $db->numRows($res);
+                            echo $num;
+                             ?></h3>
+                            <p>விழாகஂகளஂ</p>
+                        </div>
+                        <div class="icon"><i class="fa fa-"></i></div>
+                        <a href="functions.php" class="small-box-footer">மேலும் அறிக <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-xs-6">
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                        <h3><?php
+                            $sql = "SELECT SUM(amount) AS total FROM moi";
+                            $db->sql($sql);
+                            $res = $db->getResult();
+                            $num = $res[0]['total'];
+                            echo 'Rs. '.$num;
+                             ?></h3>
+                            <p>மொய்</p>
+                        </div>
+                        <div class="icon"><i class="fa fa-money"></i></div>
+                        <a href="mois.php" class="small-box-footer">மேலும் அறிக <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <script>
+        $('#filter_order').on('change', function() {
+            $('#orders_table').bootstrapTable('refresh');
+        });
+        $('#seller_id').on('change', function() {
+            $('#orders_table').bootstrapTable('refresh');
+        });
+    </script>
+    <script>
+        function queryParams(p) {
+            return {
+                "filter_order": $('#filter_order').val(),
+                "seller_id": $('#seller_id').val(),
+                limit: p.limit,
+                sort: p.sort,
+                order: p.order,
+                offset: p.offset,
+                search: p.search
+            };
+        }
+
+    </script>
+    <?php include "footer.php"; ?>
+</body>
+</html>
