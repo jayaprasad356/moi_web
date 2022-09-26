69ed5fcffa482e20871a6c80a248386fe24c5ff0
divaharsha <divakarvan03@gmail.com>
2022-09-26 09:23:53 +0530

    updateuser api updated

---
+++ api/updateuser.php
@@ -0,0 +1,71 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User Id is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+if (empty($_POST['name'])) {
+    $response['success'] = false;
+    $response['message'] = "Name is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+if (empty($_POST['mobile'])) {
+    $response['success'] = false;
+    $response['message'] = "Mobilenumber is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+if (empty($_POST['location'])) {
+    $response['success'] = false;
+    $response['message'] = "Location is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+$user_id = $db->escapeString($_POST['user_id']);
+$name = $db->escapeString($_POST['name']);
+$mobile = $db->escapeString($_POST['mobile']);
+$location = $db->escapeString($_POST['location']);
+
+
+$sql = "SELECT * FROM users WHERE id = '$user_id'";
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+if ($num == 1) {
+    $sql = "UPDATE users SET name='$name',mobile='$mobile',location='$location' WHERE id='$user_id'";
+    $db->sql($sql);
+    $sql = "SELECT * FROM users WHERE id = '$user_id'";
+    $db->sql($sql);
+    $res = $db->getResult();
+    $response['success'] = true;
+    $response['message'] = "User Updated Successfully";
+    $response['data'] = $res;
+    print_r(json_encode($response));
+    return false;
+
+}
+else{
+    $response['success'] = false;
+    $response['message'] ="User Not Found";
+    print_r(json_encode($response));
+    return false;
+
+}
+
+?>
