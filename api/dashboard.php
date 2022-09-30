2193e2e674e872363cde245353dfbd27b685dccd
jayaprasad356 <jayaprasad356@gmail.com>
2022-10-01 03:24:28 +0530

    dashboard api updated

--- api/dashboard.php
+++ api/dashboard.php
@@ -30,31 +30,31 @@ if ($num == 1) {
     $sql = "SELECT SUM(amount) AS debitmoi FROM moi WHERE user_id = '$user_id'";
     $db->sql($sql);
     $res = $db->getResult();
-    $debitmoi = $res[0]['debitmoi'];
+    $debitmoi = ($res[0]['debitmoi'] == 'NULL') ? $res[0]['debitmoi'] : "0";
     $sql = "SELECT SUM(amount) AS creditmoi FROM moi WHERE organizer_id = '$user_id'";
     $db->sql($sql);
     $res = $db->getResult();
-    $creditmoi = $res[0]['creditmoi'];
+    $creditmoi = ($res[0]['creditmoi'] == 'NULL') ? $res[0]['creditmoi'] : "0";
 
-    $sql = "SELECT * FROM functions WHERE user_id='$user_id' LIMIT 10";
-    $db->sql($sql);
-    $res = $db->getResult();
-    $rows = array();
-    foreach ($res as $row) {
-        $temp['id'] = $row['id'];
-        $temp['user_id'] = $row['user_id'];
-        $temp['function_name'] = $row['function_name'];
-        $temp['image'] = DOMAIN_URL . $row['image'];
-        $temp['place'] = $row['place'];
-        $temp['date'] = $row['date'];
-        $rows[] = $temp;
+    // $sql = "SELECT * FROM functions WHERE user_id='$user_id' ORDER BY id DESC LIMIT 10";
+    // $db->sql($sql);
+    // $res = $db->getResult();
+    // $rows = array();
+    // foreach ($res as $row) {
+    //     $temp['id'] = $row['id'];
+    //     $temp['user_id'] = $row['user_id'];
+    //     $temp['function_name'] = $row['function_name'];
+    //     $temp['image'] = DOMAIN_URL . $row['image'];
+    //     $temp['place'] = $row['place'];
+    //     $temp['date'] = $row['date'];
+    //     $rows[] = $temp;
         
-    }
+    // }
     $response['success'] = false;
     $response['message'] ="Dashboard Retrived Successfully";
     $response['debitmoi'] = $debitmoi;
     $response['creditmoi'] = $creditmoi;
-    $response['recent_functions'] = $rows;
+    // $response['recent_functions'] = $rows;
     print_r(json_encode($response));
     return false;
 }
